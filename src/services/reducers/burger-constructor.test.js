293c085a09bf144e87972f1ae2c44e5a93f33d6e
Aleksei <aleksei.usoff@gmail.com>
2023-04-07 21:01:26 +0300

    sprint-6/step-1

--- src/services/reducers/burger-constructor.test.js
+++ src/services/reducers/burger-constructor.test.js
@@ -3,6 +3,11 @@ import { burgerConstructorReducer } from "./burger-constructor"
 
 
 describe("Burger-constructor", () => {
+    const initialState = {
+        bun: null,
+        constructorIngridients: [],
+        totalPrice: 0,
+    }
 
     it('test initial state', () => {
         expect(burgerConstructorReducer(undefined, {})).toEqual({
@@ -13,11 +18,6 @@ describe("Burger-constructor", () => {
     })
 
     it('test add bun', () => {
-        const initialState = {
-            bun: null,
-            constructorIngridients: [],
-            totalPrice: 0,
-        }
         const bun = { type: 'bun' }
         const action = { type: ADD_BUN, ingridient: bun }
         expect(burgerConstructorReducer(initialState, action)).toEqual({
@@ -27,11 +27,6 @@ describe("Burger-constructor", () => {
     })
 
     it('test add ingridient', () => {
-        const initialState = {
-            bun: null,
-            constructorIngridients: [],
-            totalPrice: 0,
-        }
         let result;
         const ingridient = { type: 'main' }
         const action = { type: ADD_INGRIDIENT, ingridient: ingridient }
@@ -46,7 +41,7 @@ describe("Burger-constructor", () => {
     })
 
     it('test remove ingridient', () => {
-        const initialState = {
+        const currentInitialState = {
             bun: { type: 'bun' },
             constructorIngridients: [
                 { type: 'main', uniqid: '12345' },
@@ -56,7 +51,7 @@ describe("Burger-constructor", () => {
         }
         let result;
         const action = { type: REMOVE_INGRIDIENT, ingridient: { type: 'main', uniqid: '12345' } }
-        result = burgerConstructorReducer(initialState, action);
+        result = burgerConstructorReducer(currentInitialState, action);
         expect(result.constructorIngridients.length).toBe(1)
 
         const action2 = { type: REMOVE_INGRIDIENT, ingridient: { type: 'sauce', uniqid: '67890' } }
@@ -66,7 +61,7 @@ describe("Burger-constructor", () => {
 
 
     it('test total price', () => {
-        const initialState = {
+        const currentInitialState = {
             bun: { type: 'bun', price: 500 },
             constructorIngridients: [
                 { type: 'main', uniqid: '12345', price: 1000 },
@@ -76,13 +71,13 @@ describe("Burger-constructor", () => {
         }
         let result;
         const action = { type: SET_TOTALPRICE }
-        result = burgerConstructorReducer(initialState, action);
+        result = burgerConstructorReducer(currentInitialState, action);
         expect(result.totalPrice).toBe(2500)
     })
 
 
     it('sort ingridients', () => {
-        const initialState = {
+        const currentInitialState = {
             bun: { type: 'bun', price: 500 },
             constructorIngridients: [
                 { type: 'main', uniqid: '12345', price: 1000 },
@@ -92,7 +87,7 @@ describe("Burger-constructor", () => {
             totalPrice: 0,
         }
         const action = { type: SORT_INGRIDIENTS, rest: { from: 1, to: 2 } }
-        const result = burgerConstructorReducer(initialState, action);
+        const result = burgerConstructorReducer(currentInitialState, action);
         expect(result.constructorIngridients).toEqual([
             { type: 'main', uniqid: '12345', price: 1000 },
             { type: 'sauce', uniqid: '34245', price: 700 },
